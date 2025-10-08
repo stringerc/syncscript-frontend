244b5be19e1816b0b5d42d096097b44a93683ce8
stringerc <stringer.c.a@gmail.com>
2025-10-08 18:15:18 -0400

    fix: Request Auth0 API audience and scopes during login

--- pages/api/auth/[...auth0].ts
+++ pages/api/auth/[...auth0].ts
@@ -2,7 +2,11 @@ import { handleAuth, handleLogin, handleLogout } from '@auth0/nextjs-auth0';
 
 export default handleAuth({
   login: handleLogin({
-    returnTo: '/dashboard'
+    returnTo: '/dashboard',
+    authorizationParams: {
+      audience: process.env.AUTH0_AUDIENCE,
+      scope: 'openid profile email read:users write:users read:tasks write:tasks read:energy write:energy read:projects write:projects'
+    }
   }),
   logout: handleLogout({
     returnTo: '/'
