88dbdc3bd04af35b21951bab0082cc43ad0a662c
stringerc <stringer.c.a@gmail.com>
2025-10-08 23:16:01 -0400

    fix: Remove favicon.ico fallback reference to prevent 404 error
    - Remove link to non-existent favicon.ico
    - Use SVG favicon exclusively
    - Add sizes='any' for better browser support
    - Add theme-color meta tag for mobile browsers
    - Browsers will stop requesting .ico file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,8 +5,9 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="alternate icon" href="/favicon.ico" />
+        <link rel="icon" sizes="any" href="/favicon.svg" />
         <meta name="description" content="SyncScript - The Seamless Productivity Experience" />
+        <meta name="theme-color" content="#4A90E2" />
       </Head>
       <body>
         <Main />
